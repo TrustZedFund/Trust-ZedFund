cdbfaea25c852ca8029f56235ceff2c31405584e
TrustZedFund <trustzedfund@gmail.com>
2026-01-27 04:54:40 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,15 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtcHAOGt-veevCcbOY5i-wotqMGqlIkhE",
-  authDomain: "trustvault-3cf8f.firebaseapp.com",
-  databaseURL: "https://trustvault-3cf8f-default-rtdb.firebaseio.com",
-  projectId: "trustvault-3cf8f",
-  storageBucket: "trustvault-3cf8f.firebasestorage.app",
-  messagingSenderId: "304860923047",
-  appId: "1:304860923047:web:79d4ea96946c728a9e4782"
+  apiKey: "AIzaSyDvkMDvK5d7P7p2zatUjIsJNGhBf18yeTQ",
+  authDomain: "trust-zedfund.firebaseapp.com",
+  databaseURL: "https://trust-zedfund-default-rtdb.firebaseio.com",
+  projectId: "trust-zedfund",
+  storageBucket: "trust-zedfund.firebasestorage.app",
+  messagingSenderId: "129257684900",
+  appId: "1:129257684900:web:95e94293366a26f9448b31"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 
-console.log("🔥 Firebase (Realtime DB) ready");
+console.log("🔥 Trust ZedFund Firebase (Realtime DB) ready");
