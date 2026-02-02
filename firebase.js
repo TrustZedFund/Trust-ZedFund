135c2b12f38b477c06fb9ef4921c97737d24c589
TrustZedFund <trustzedfund@gmail.com>
2026-02-02 14:54:11 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -493,3 +493,42 @@ export function formatDate(timestamp) {
 export { auth, db };
 export { onAuthStateChanged };
 export { ref, set, update, get, push, remove, onValue };
+/* ===================================================
+   ADD THESE EXPORTS TO YOUR EXISTING firebase.js
+   (Add to the bottom of the file)
+=================================================== */
+
+// Make sure to export onAuthStateChanged from auth
+export { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+// Also export all database functions you might need
+export {
+  ref,
+  set,
+  update,
+  get,
+  onValue,
+  push,
+  remove,
+  query,
+  orderByChild,
+  equalTo
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
+
+// Export auth functions
+export {
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+// Helper function for investment page
+export async function getCurrentUser() {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
+}
