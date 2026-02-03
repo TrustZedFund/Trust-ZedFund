547a2f2cfb382722573008cc7dfa172fb2d7e37a
TrustZedFund <trustzedfund@gmail.com>
2026-02-03 17:37:34 +0200

    Update register.js

--- register.js
+++
@@ -1,3 +0,0 @@
-import { auth, db } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-import { push, ref } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
