36e77329537a0e8b26d9d8ef7c833e1524f9ce52
TrustZedFund <trustzedfund@gmail.com>
2026-02-03 16:18:30 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -24,6 +24,18 @@ import {
   getDownloadURL
 } from
   "https://www.gstatic.com/firebasejs/12.8.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  // YOUR CONFIG
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
 
 /* ================= FIREBASE CONFIG ================= */
 
